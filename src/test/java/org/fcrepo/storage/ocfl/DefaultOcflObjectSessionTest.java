bd4e62bc05f6836d30e0ca50ed3963d042043118
dbernstein <dabylon@gmail.com>
2022-01-26 10:11:08 -0800

    Upgrades ocfl-java to 1.4.3 (#49)
    Resolves: https://fedora-repository.atlassian.net/browse/FCREPO-3793

--- src/test/java/org/fcrepo/storage/ocfl/DefaultOcflObjectSessionTest.java
+++ src/test/java/org/fcrepo/storage/ocfl/DefaultOcflObjectSessionTest.java
@@ -17,7 +17,7 @@ import edu.wisc.library.ocfl.api.model.ObjectVersionId;
 import edu.wisc.library.ocfl.core.OcflRepositoryBuilder;
 import edu.wisc.library.ocfl.core.extension.storage.layout.config.HashedNTupleLayoutConfig;
 import edu.wisc.library.ocfl.core.path.mapper.LogicalPathMappers;
-import edu.wisc.library.ocfl.core.storage.filesystem.FileSystemOcflStorage;
+import edu.wisc.library.ocfl.core.storage.OcflStorageBuilder;
 import org.apache.commons.lang3.SystemUtils;
 import org.fcrepo.storage.ocfl.cache.CaffeineCache;
 import org.fcrepo.storage.ocfl.cache.NoOpCache;
@@ -107,7 +107,7 @@ public class DefaultOcflObjectSessionTest {
         ocflRepo = new OcflRepositoryBuilder()
                 .defaultLayoutConfig(new HashedNTupleLayoutConfig())
                 .logicalPathMapper(logicalPathMapper)
-                .storage(FileSystemOcflStorage.builder().repositoryRoot(ocflRoot).build())
+                .storage(OcflStorageBuilder.builder().fileSystem(ocflRoot).build())
                 .workDir(ocflTemp)
                 .buildMutable();
 
